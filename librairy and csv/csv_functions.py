e6191cf10c4dc3e8365dc768ae227e493499b35c
ZeelX <87006085+ZeelX@users.noreply.github.com>
2024-01-23 13:46:08 +0100

    add better return

--- librairy and csv/csv_functions.py
+++ librairy and csv/csv_functions.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 
 def add_function(number1, number2):
-
     """
     make addition with 2 number
     :param number1:
@@ -13,9 +12,8 @@ def add_function(number1, number2):
 
 
 number_calculated = 25
-
-result_list = [add_function(number_calculated, x) for x in range(0, 75, 3)]
-data = pd.DataFrame({'Result': result_list})
+result_list = {x: add_function(number_calculated, x) for x in range(0, 75, 3)}
+data = pd.DataFrame({'calcul': f'+ {number_calculated} =', 'Result': result_list})
 
 print(data)
 data.to_csv(f'DATA/Result_for_{number_calculated}.csv', index=False)
